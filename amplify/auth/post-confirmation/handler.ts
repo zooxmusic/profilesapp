5b50c0e00c1c7c27e2c1e741c6e32acfc4ec1fdc
Brian Szucs <brian@zooxmusic.com>
2025-02-03 19:20:25 -0500

    fixed dmplify

---
+++ amplify/auth/post-confirmation/handler.ts
@@ -0,0 +1,5 @@
+import type { PostConfirmationTriggerHandler } from "aws-lambda";
+
+export const handler: PostConfirmationTriggerHandler = async (event) => {
+  return event;
+};
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,27 +1,56 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { postConfirmation } from "../auth/post-confirmation/resource";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
-});
+const schema = a
+  .schema({
+    UserProfile: a
+      .model({
+        email: a.string(),
+        profileOwner: a.string(),
+      })
+      .authorization((allow) => [
+        allow.ownerDefinedIn("profileOwner"),
+      ]),
+  })
+  .authorization((allow) => [allow.resource(postConfirmation)]);
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
+
+
+// import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+
+// /*== STEP 1 ===============================================================
+// The section below creates a Todo database table with a "content" field. Try
+// adding a new "isDone" field as a boolean. The authorization rule below
+// specifies that any unauthenticated user can "create", "read", "update", 
+// and "delete" any "Todo" records.
+// =========================================================================*/
+// const schema = a.schema({
+//   Todo: a
+//     .model({
+//       content: a.string(),
+//     })
+//     .authorization((allow) => [allow.guest()]),
+// });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     defaultAuthorizationMode: 'iam',
+//   },
+// });
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
